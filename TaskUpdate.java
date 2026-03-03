178b69dd17bcd7993d72fda9de2b4e7579bce159
ShobhanaMangale <shobhanadmangale5@gmail.com>
2026-03-03 17:13:23 +0530

    Create TaskUpdate.java

---
+++ TaskUpdate.java
@@ -0,0 +1,14 @@
+public class TaskUpdate
+  {
+    private String status="Pending;
+
+    public void updateStatus(String newStatus)
+    {
+     this.status=newStatus; 
+    }
+
+    public String getStatus()
+    {
+      return status;
+    }
+  }
